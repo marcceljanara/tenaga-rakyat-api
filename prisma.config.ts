07e17e629afca206f544fbf46a02802d7a8a6390
Marccel Janara <nengahyuichiro34@gmail.com>
2026-03-29 16:49:59 +0700

    update prisma configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 import 'dotenv/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-    seed: 'ts-node prisma/seed.ts',
+    seed: process.env.NODE_ENV === 'production' ? 'node dist/prisma/seed.js' : 'ts-node prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL,
   },
 });
